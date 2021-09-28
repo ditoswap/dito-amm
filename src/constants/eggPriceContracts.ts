6e3785d308629c7231c51a5699b96fb783db900e
이진우 <binarywoo@gmail.com>
2021-09-28 21:45:10 +0900

    update contract address

--- src/constants/eggPriceContracts.ts
+++ src/constants/eggPriceContracts.ts
@@ -1,7 +1,7 @@
 const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
   cakeAddress: '0xAAC271161c2a8283DfAAbb1b9696C6EFF031e980',
   busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
-  lpAddress: '0x4ac44c09c61c2e61470a84eb5cf169d5c8b15521'
+  lpAddress: '0x81a9cb5E45740d8de7B4e0E094Ed2b40304b3eCf'
 }
 
 export default priceContracts
